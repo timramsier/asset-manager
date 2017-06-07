af3613dbfe57025b99cc0bd17ce83a23945e53e0
Tim Ramsier <tim.ramsier@iacpublishinglabs.com>
2017-06-07 10:49:12 -0700

    Add BasicAuth to the API and add necessary FrontEnd code
    * Setup BasicAuth for API routes
    * Integrated BasicAuth into axios calls for FE

--- api/routes/api.router.js
+++ api/routes/api.router.js
@@ -1,20 +1,21 @@
 const express = require('express')
-const assets = require('./assets.router')
 const mongoose = require('mongoose')
+const assets = require('./assets.router')
+const mainController = require('../js/main.controller')
+const userController = require('../auth/controllers/user')
+const authController = require('../auth/controllers/auth')
 const { categorySchema } = require('../js/schema')
 
 const router = express.Router()
 var db = {}
 db.Category = mongoose.model('Category', categorySchema)
 
-router.get('/category', (req, res) => {
-  db.Category
-  .find({}, 'name label description config')
-  .exec((err, result) => {
-    if (err) res.send(err)
-    res.send(JSON.stringify(result))
-  })
-})
+router.route('/category')
+  .get(authController.isAuthenticated, mainController.getCategories)
+
+router.route('/users')
+  .post(userController.postUsers)
+  .get(userController.getUsers)
 
 router.use('/assets', assets)
 
