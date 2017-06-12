256ff055cb30ee59597056c39b9db1aa42c461dc
Tim Ramsier <tim.ramsier@iacpublishinglabs.com>
2017-06-12 14:06:19 -0700

    Add `User` database search support for assets

--- api/routes/assets.router.js
+++ api/routes/assets.router.js
@@ -1,14 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const assetController = require('../js/asset.controller')
 const authController = require('../auth/controllers/auth')
-const { modelSchema, categorySchema, assetSchema } = require('../js/schema')
 const router = express.Router()
-
-var db = {}
-db.Category = mongoose.model('Category', categorySchema)
-db.Model = mongoose.model('Model', modelSchema)
-db.Assets = mongoose.model('Asset', assetSchema)
 
 router.route('/')
   .get(authController.isAuthenticated, assetController.getAssets)
