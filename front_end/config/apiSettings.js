77dcb35a4e276cd6473d96de6739528a49ddc49b
Tim Ramsier <tim.ramsier@iacpublishinglabs.com>
2017-07-12 07:08:54 -0700

    update api config for frontend

--- front_end/config/apiSettings.js
+++ front_end/config/apiSettings.js
@@ -1,9 +1,8 @@
 const apiSettings = {
-  uri: 'localhost:3000/api/beta',
+  uri: process.env.APP_FRONTEND_API_URI || 'localhost:3000/api/beta',
   auth: {
-    username: 'ecfd0226-bce7-4737-c4f3-cdcff87bda68',  // this is the api key
+    username: process.env.APP_FRONTEND_API_KEY || 'ecfd0226-bce7-4737-c4f3-cdcff87bda68',  // this is the api key
     password: 'x'
   }
 }
-
 export default apiSettings
