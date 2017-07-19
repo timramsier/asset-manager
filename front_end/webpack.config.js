dc19685109108c8ba81d5e3df29d97de955c1422
Tim Ramsier <tim.ramsier@iacpublishinglabs.com>
2017-07-19 07:41:12 -0700

    Renamve AsyncRoute to AsyncLoad and refactor LeftNavbar to use it
    * Add stylized loading animation

--- front_end/webpack.config.js
+++ front_end/webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.json']
+    // alias: {
+    //   react: 'preact-compat',
+    //   'react-dom': 'preact-compat'
+    // }
   },
   stats: {
     colors: true,
@@ -47,7 +51,10 @@ module.exports = {
         loader: 'json-loader'
       },
       {
-        include: path.resolve(__dirname, 'js'),
+        include: [
+          path.resolve(__dirname, 'js'),
+          path.resolve('node_modules/preat-compat/src')
+        ],
         test: /\.js$/,
         loader: 'babel-loader'
       },
