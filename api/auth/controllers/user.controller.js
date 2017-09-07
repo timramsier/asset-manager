58fba95612a468157fdc8812105368b164cd026f
Tim Ramsier <tim.ramsier@iacpublishinglabs.com>
2017-09-07 10:11:12 -0700

    Add login in route and functionality to API

--- api/auth/controllers/user.controller.js
+++ api/auth/controllers/user.controller.js
@@ -22,3 +22,22 @@ exports.postUsers = function (req, res) {
 
 // Create endpoint /api/users for GET
 exports.getUsers = _controller(User).getAll
+
+// Validate password
+exports.login = (req, res, next) => {
+  console.log(req.body)
+  const { _id, password } = req.body
+  _controller(User, {}, { _id })
+  .getOne(req, res, next, (err, user) => {
+    if (err) return res.send(err).status(500)
+    user.verifyPassword(password, function (err, isMatch) {
+      if (err) { return res.send(err).status(500) }
+
+      // Password did not match
+      if (!isMatch) { return res.sendStatus(401) }
+
+      // Success
+      return res.send(user).status(200)
+    })
+  })
+}
