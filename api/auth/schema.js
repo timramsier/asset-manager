296e0c668589d4568fde4080250956d7a710b6f9
Tim Ramsier <tim.ramsier@dictionary.com>
2017-10-09 22:32:53 -0700

    Add add/update user functionality

--- api/auth/schema.js
+++ api/auth/schema.js
@@ -1,61 +1,75 @@
-const mongoose = require('mongoose')
-var bcrypt = require('bcrypt-nodejs')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+const shortId = require('shortid');
 
 const userSchema = Schema({
+  _shortId: {
+    type: String,
+    unique: true,
+    default: shortId.generate,
+  },
   username: {
     type: String,
     unique: true,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    unique: true
+    unique: true,
   },
   firstName: String,
   lastName: String,
   password: {
     type: String,
-    required: true
+    required: true,
   },
   accessLevel: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+  lastModifiedBy: {
+    type: String,
+    ref: 'User',
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 // Execute before each user.save() call
-userSchema.pre('save', function (callback) {
-  var user = this
+userSchema.pre('save', function(callback) {
+  var user = this;
 
   // Break out if the password hasn't changed
-  if (!user.isModified('password')) return callback()
+  if (!user.isModified('password')) return callback();
 
   // Password changed so we need to hash it
-  bcrypt.genSalt(5, function (err, salt) {
-    if (err) return callback(err)
+  bcrypt.genSalt(5, function(err, salt) {
+    if (err) return callback(err);
 
-    bcrypt.hash(user.password, salt, null, function (err, hash) {
-      if (err) return callback(err)
-      user.password = hash
-      callback()
-    })
-  })
-})
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return callback(err);
+      user.password = hash;
+      callback();
+    });
+  });
+});
 
-userSchema.methods.verifyPassword = function (password, cb) {
-  bcrypt.compare(password, this.password, function (err, isMatch) {
-    if (err) return cb(err)
-    cb(null, isMatch)
-  })
-}
+userSchema.methods.verifyPassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 userSchema.index({
-  'accessLevel': 'text',
-  'email': 'text',
-  'firstName': 'text',
-  'lastName': 'text',
-  'username': 'text'
-})
+  accessLevel: 'text',
+  email: 'text',
+  firstName: 'text',
+  lastName: 'text',
+  username: 'text',
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
--- api/js/schema.js
+++ api/js/schema.js
@@ -1,68 +1,68 @@
-const mongoose = require('mongoose')
-const shortId = require('shortid')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+const Schema = mongoose.Schema;
 
 // Schema
 const assetSchema = Schema({
-  _shortId: {type: String, unique: true, default: shortId.generate},
-  _parent: {type: String, required: true, ref: 'Model'},
-  assetTag: {type: String, default: ''},
-  assignedTo: {type: String, ref: 'User', default: null},
-  status: {type: String, default: 'new'},
-  sn: {type: String, required: true, default: ''},
+  _shortId: { type: String, unique: true, default: shortId.generate },
+  _parent: { type: String, required: true, ref: 'Model' },
+  assetTag: { type: String, default: '' },
+  assignedTo: { type: String, ref: 'User', default: null },
+  status: { type: String, default: 'new' },
+  sn: { type: String, required: true, default: '' },
   po: { type: String, ref: 'Po' },
   lastModifiedBy: { type: String, ref: 'User' },
-  lastModified: { type: Date, default: Date.now }
-})
+  lastModified: { type: Date, default: Date.now },
+});
 
 const specSchema = Schema({
-  _shortId: {type: String, unique: true, default: shortId.generate},
+  _shortId: { type: String, unique: true, default: shortId.generate },
   key: String,
-  value: String
-})
+  value: String,
+});
 
 const categorySchema = Schema({
-  _shortId: {type: String, unique: true, default: shortId.generate},
-  name: {type: String, required: true},
-  label: {type: String, required: true},
+  _shortId: { type: String, unique: true, default: shortId.generate },
+  name: { type: String, required: true },
+  label: { type: String, required: true },
   description: String,
   config: {
-    faIcon: {type: String, required: true},
-    color: {type: String, required: true},
-    api: {type: String, required: true},
-    fallbackImage: {type: String, required: true}
+    faIcon: { type: String, required: true },
+    color: { type: String, required: true },
+    api: { type: String, required: true },
+    fallbackImage: { type: String, required: true },
   },
-  models: [ { type: Schema.Types.ObjectId, ref: 'Model' } ],
+  models: [{ type: Schema.Types.ObjectId, ref: 'Model' }],
   lastModifiedBy: { type: String, ref: 'User' },
-  lastModified: { type: Date, default: Date.now }
-})
+  lastModified: { type: Date, default: Date.now },
+});
 
 const modelSchema = Schema({
-  _parent: {type: String, required: true, ref: 'Category'},
-  _shortId: {type: String, unique: true, default: shortId.generate},
-  category: {type: String, required: true},
+  _parent: { type: String, required: true, ref: 'Category' },
+  _shortId: { type: String, unique: true, default: shortId.generate },
+  category: { type: String, required: true },
   vendor: String,
-  name: {type: String, required: true},
+  name: { type: String, required: true },
   version: String,
   image: String,
   description: String,
-  active: {type: Boolean, default: false},
-  specs: [ specSchema ],
-  assets: [ { type: Schema.Types.ObjectId, ref: 'Asset' } ],
+  active: { type: Boolean, default: false },
+  specs: [specSchema],
+  assets: [{ type: Schema.Types.ObjectId, ref: 'Asset' }],
   lastModifiedBy: { type: String, ref: 'User' },
-  lastModified: { type: Date, default: Date.now }
-})
+  lastModified: { type: Date, default: Date.now },
+});
 
 const poSchema = Schema({
-  _shortId: {type: String, unique: true, default: shortId.generate},
-  poNumber: {type: String, required: true},
-  bu: {type: String, required: true},
+  _shortId: { type: String, unique: true, default: shortId.generate },
+  poNumber: { type: String, required: true },
+  bu: { type: String, required: true },
   lastModifiedBy: { type: String, ref: 'User' },
   lastModified: { type: Date, default: Date.now },
   createdBy: { type: String, ref: 'User' },
   created: { type: Date, default: Date.now },
-  assets: [ { type: Schema.Types.ObjectId, ref: 'Asset' } ]
-})
+  assets: [{ type: Schema.Types.ObjectId, ref: 'Asset' }],
+});
 
 // indexes
 
@@ -71,16 +71,16 @@ assetSchema.index({
   status: 'text',
   sn: 'text',
   po: 'text',
-  assignedTo: 'text'
-})
+  assignedTo: 'text',
+});
 
 modelSchema.index({
   vendor: 'text',
   name: 'text',
   version: 'text',
   description: 'text',
-  category: 'text'
-})
+  category: 'text',
+});
 
 categorySchema.index({
   name: 'text',
@@ -88,19 +88,19 @@ categorySchema.index({
   description: 'text',
   'config.faIcon': 'text',
   'config.color': 'text',
-  'config.api': 'text'
-})
+  'config.api': 'text',
+});
 
 poSchema.index({
   poNumber: 'text',
-  bu: 'text'
-})
+  bu: 'text',
+});
 
 // add property to allow 'search' url query usage in _.controller.js
-assetSchema._custom = { textIndex: true }
-modelSchema._custom = { textIndex: true }
-categorySchema._custom = { textIndex: true }
-poSchema._custom = { textIndex: true }
+assetSchema._custom = { textIndex: true };
+modelSchema._custom = { textIndex: true };
+categorySchema._custom = { textIndex: true };
+poSchema._custom = { textIndex: true };
 
 module.exports = {
   assetSchema,
@@ -110,5 +110,5 @@ module.exports = {
   modelModel: mongoose.model('Model', modelSchema),
   assetModel: mongoose.model('Asset', assetSchema),
   categoryModel: mongoose.model('Category', categorySchema),
-  poModel: mongoose.model('Po', poSchema)
-}
+  poModel: mongoose.model('Po', poSchema),
+};
